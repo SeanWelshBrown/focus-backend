ea409d841b3bc968cee762a08393b3ceb278b4f8
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-04-02 15:18:36 -0400

    Removed index routes of models for security/privacy purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:create, :update, :index]
-  resources :meditation_sessions, only: [:create, :index, :destroy]
-  resources :focus_sessions, only: [:create, :index, :destroy]
+  resources :users, only: [:create, :update]
+  resources :meditation_sessions, only: [:create, :destroy]
+  resources :focus_sessions, only: [:create, :destroy]
 
   post "/login", to: "users#login"
 
